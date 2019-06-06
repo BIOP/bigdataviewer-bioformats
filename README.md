df1956a363930e9206d9c4a476a0bc46a31152eb
Nicolas Chiaruttini <nicolas.chiaruttini@gmail.com>
2019-06-06 11:03:24 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This repository's goal is to enable the opening of files into BigDataViewer
+by using the BioFormats API.
+
+2D VSI Files have been tested. Hopefully more are to come.
+
---
+++ src/test/java/SimpleIJLaunch.java
@@ -0,0 +1,12 @@
+import net.imagej.ImageJ;
+import org.junit.Test;
+
+public class SimpleIJLaunch {
+
+    static public void main(String... args) {
+        // Arrange
+        // create the ImageJ application context with all available services
+        final ImageJ ij = new ImageJ();
+        ij.ui().showUI();
+    }
+}
