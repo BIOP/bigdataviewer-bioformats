10c28db12d0a600a62578e7c83133b0da75d0fd6
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2019-11-11 01:07:03 +0100

    adapts command name to SciJava Bdv repo

--- src/main/java/ch/epfl/biop/bdv/bioformats/command/ConvertFilesToBdvXmlDatasetCommand.java
+++ src/main/java/ch/epfl/biop/bdv/bioformats/command/ConvertFilesToBdvXmlDatasetCommand.java
@@ -2,11 +2,7 @@ package ch.epfl.biop.bdv.bioformats.command;
 
 import ch.epfl.biop.bdv.bioformats.bioformatssource.BioFormatsBdvOpener;
 import ch.epfl.biop.bdv.bioformats.export.spimdata.BioFormatsConvertFilesToSpimData;
-import mpicbg.spim.data.SpimData;
-import mpicbg.spim.data.SpimDataException;
-import mpicbg.spim.data.XmlIoSpimData;
 import mpicbg.spim.data.generic.AbstractSpimData;
-import org.apache.commons.io.FilenameUtils;
 import org.scijava.ItemIO;
 import org.scijava.command.Command;
 import org.scijava.plugin.Parameter;
@@ -16,7 +12,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-@Plugin(type = Command.class,menuPath = "BDV_SciJava>Export>Convert Files to Bdv Dataset with Bioformats (SciJava)")
+@Plugin(type = Command.class,menuPath = "BDV_SciJava>SpimDataset>Open>SpimDataset [BioFormats Bdv Bridge]")
 public class ConvertFilesToBdvXmlDatasetCommand extends BioformatsBigdataviewerBridgeDatasetCommand {
 
     @Parameter
@@ -25,26 +21,11 @@ public class ConvertFilesToBdvXmlDatasetCommand extends BioformatsBigdataviewerB
     @Parameter(type = ItemIO.OUTPUT)
     AbstractSpimData spimData;
 
-    @Parameter
-    boolean saveDataset = false;
-
-    @Parameter(required=false)
-    File datasetFileName;
-
     public void run() {
         List<BioFormatsBdvOpener> openers = new ArrayList<>();
         for (File f:files) {
             openers.add(getOpener(f));
         }
         spimData = BioFormatsConvertFilesToSpimData.getSpimData(openers);
-        if (saveDataset) {
-            spimData.setBasePath(datasetFileName.getParentFile());
-            try {
-                String renamedXmlFileName = FilenameUtils.removeExtension(datasetFileName.getAbsolutePath())+".xml";
-                new XmlIoSpimData().save( (SpimData) spimData, renamedXmlFileName );
-            } catch (SpimDataException e) {
-                e.printStackTrace();
-            }
-        }
     }
 }
